2dedc70adae01ab86262eb6728215855d387352d
Erin Shaben <eshaben@icloud.com>
2017-07-13 21:45:41 -0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
-console.log('Hello');
+app.use(bodyParser.json())
 
 app.get('/math/pi', function(request, response){
   response.send(String(Math.PI))
@@ -39,6 +40,27 @@ app.post('/math/sum', function(request, response){
   }
   query = query.join('+')
   response.send(query + "=" + total);
+})
+
+app.use('/math/volume', function(request, response){
+  var query = request.path
+  var numbers = []
+  var result = 1
+  for (var i=0; i<query.length; i++){
+  if (isNaN(query[i]) == false){
+      numbers.push(query[i])
+    };
+  }
+  for (var i=0; i<numbers.length; i++){
+    result *= numbers[i]
+  }
+  var stringAnswer = "The volume of a " + numbers.join('x') + " rectangle is " + result
+  response.send(stringAnswer);
+})
+
+
+app.post('/math/area', function(req, response){
+  console.log(req.body)
 })
 
 app.listen(8080)
